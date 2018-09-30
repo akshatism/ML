75670b8cbeccb14ce4c2819fab7ef211453cba3b
akshatism <akshatkumar.2013@gmail.com>
2018-09-30 00:23:48 -0400

    :sparkles: Adding plots of Cost Function and Hypothesis Function

--- algorithms/Linear-Regression-Single-Feature/linear_regression_one_variable_using_package.py
+++ algorithms/Linear-Regression-Single-Feature/linear_regression_one_variable_using_package.py
@@ -1,8 +1,10 @@
+import config
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import linear_model
-import matplotlib.pyplot as plt
 
-data = pd.read_csv("/Users/akshatkumar/Downloads/machine-learning-ex1/ex1/ex1data1.txt")
+# Load data using read_csv method
+data = pd.read_csv(config.path)
 
 X = data['population']
 Y = data['profit']
@@ -10,25 +12,30 @@ Y = data['profit']
 X = X.values.reshape(len(X), 1)
 Y = Y.values.reshape(len(Y), 1)
 
-X_train = X[:67]
-Y_train = Y[:67]
+# Divide into training and testing data
+X_train = X[:config.train_data]
+Y_train = Y[:config.train_data]
 
+# Scatter Plot of training data Population vs Profit
 plt.scatter(X_train, Y_train)
 plt.title('Linear Regression Train Data')
 plt.xlabel('Population')
 plt.ylabel('Profit')
 plt.show()
 
-X_test = X[67:]
-Y_test = Y[67:]
+X_test = X[config.test_data:]
+Y_test = Y[config.test_data:]
 
+# Scatter Plot of testing data Population vs Profit
 plt.scatter(X_test, Y_test)
 plt.title('Linear Regression Test Data')
 plt.xlabel('Population')
 plt.ylabel('Profit')
 plt.show()
 
+# import LinearRegression() from sklearn
 regression = linear_model.LinearRegression()
 regression.fit(X_test, Y_test)
 
+# Predicting and plotting test data
 plt.plot(X_test, regression.predict(X_test))
